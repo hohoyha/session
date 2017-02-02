b2f7db3f535db3167d4c5b6a0d966d95ea2b7812
hohoyha <hohoyha@hotmail.com>
2017-02-02 21:31:12 +0900

    단순 로그인 로직 처리

--- app.js
+++ app.js
@@ -2,10 +2,14 @@ var express = require('express');
 var session = require('express-session');
 var cookieParser  = require('cookie-parser');
 var count = require('./routes/count');
-var login = require('./routes/login')
+var login = require('./routes/login');
+var bodyParser = require('body-parser');
 
 var app = express();
 app.use(cookieParser());
+
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
 
 app.use(session({
   secret: 'dwqe123e11231312312@#',
@@ -15,6 +19,9 @@ app.use(session({
 
 app.get('/count', count.count);
 app.get('/auth/login', login.login);
+app.post('/auth/login', login.loginprocess)
+app.get('/welcome', login.welcome);
+app.get('/auth/logout', login.logout);
 
 app.listen(3000, function(){
     console.log('Server is starting on Port: 3000');
--- routes/login.js
+++ routes/login.js
@@ -1,6 +1,6 @@
 module.exports.login = function(req, res){
     var output = `
-     <form>
+     <form action = "/auth/login"  method ="post">
         <p>
           <input type ="text" name="username" placeholder="username">
         </p>
@@ -14,4 +14,42 @@ module.exports.login = function(req, res){
     `;
 
     res.send(output);
+}
+
+module.exports.loginprocess = function(req, res){
+   var user = {
+      username:'hoho',
+      password:'111',
+      displayname:'용호'
+   }
+
+   var username = req.body.username;
+   var pwd = req.body.password;
+
+   if(username === user.username && pwd === user.password){
+      req.session.displayname = user.displayname;
+   }
+
+   res.redirect('/welcome'); 
+}
+
+
+module.exports.welcome = function(req, res){
+    if(req.session.displayname)
+    {
+       res.send(`
+       <h1>Hello, ${req.session.displayname}<h1>
+       <a href="/auth/logout" >logout</a>`
+       );
+    }
+    else{
+       res.send(`<h1>welcome</h1>
+       <a href="/auth/login">Login</a>
+       `);     
+    }  
+}
+
+module.exports.logout = function(req, res){
+   delete req.session.displayname;
+   res.redirect('/welcome');
 }
